3ce76d4efec17e4fe2222446074df2d061b00f14
Liashui <1060704961@qq.com>
2020-10-25 21:52:01 +0800

    Initial Commit

---
+++ CATransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CATransitionDemo
+//
+//  Created by Liashui on 2020/10/25.
+//  Copyright © 2020年 Liashui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
